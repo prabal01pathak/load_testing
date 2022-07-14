451f3bdf0c91d77544243d2dbf43e206bdb0e90b
Prabal Pathak <prabal01pathak@gmail.com>
2022-07-14 20:43:47 +0530

    ** add system_details module **
    1. get processdetails as system gpu, cpu, memory details.

--- load_testing/utils/system_details.py
+++ load_testing/utils/system_details.py
@@ -2,6 +2,69 @@
 Get system details like cpu usage, gpu usage, temprature etc.
 """
 import os
+import time
+import platform
 
-import psutils
-import gputils
+import psutil
+import GPUtil as gputil
+
+
+class ProcessDetails:
+    """get process details like cpu, gpu, and memory
+    Args:
+        pid(int): process id
+    """
+
+    def __init__(self):
+        self.pid = os.getpid()
+        self.process = psutil.Process(pid=self.pid)
+        self.uname = platform.uname()
+        self.gpu = gputil.getGPUs()[0]
+
+    def system_info(self) -> dict:
+        """return device informations like os, processer
+        Return:
+            information(dict): os, release, version, processor
+                physical_cores, total_cores
+        """
+        return {
+            "os": self.uname.system,
+            "release": self.uname.release,
+            "version": self.uname.version,
+            "processer": self.uname.processor,
+            "physical cores": psutil.cpu_count(logical=False),
+            "total_cores": psutil.cpu_count(logical=True),
+        }
+
+    def get_cpu_usage(self):
+        """get cpu usage"""
+        cpu_utilization = self.process.cpu_percent()
+        return cpu_utilization
+
+    def get_gpu_usage(self):
+        """return total gpu usage"""
+        return self.gpu.memoryFree
+
+    def get_memory_usage(self):
+        """get total memory usage"""
+
+    def get_gpu_temprature(self):
+        """return gpu temprature"""
+
+    def running_time(self, func) -> float:
+        """running time decorator to caculate running time
+
+        Args:
+            func (function): function
+
+        Returns:
+            total_time(float): total processing time
+        """
+
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            func(*args, **kwargs)
+            end_time = time.time()
+            return end_time - start_time
+
+        return wrapper
