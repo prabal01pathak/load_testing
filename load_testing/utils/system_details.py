d5a642b549d4c57dbb7362229474251de86a5034
Prabal Pathak <prabal01pathak@gmail.com>
2022-07-14 22:31:50 +0530

    add gpu details in system_details.py

--- load_testing/utils/system_details.py
+++ load_testing/utils/system_details.py
@@ -19,7 +19,9 @@ class ProcessDetails:
         self.pid = os.getpid()
         self.process = psutil.Process(pid=self.pid)
         self.uname = platform.uname()
-        self.gpu = gputil.getGPUs()[0]
+        self.gpu: list = gputil.getGPUs()
+        if len(self.gpu) > 0:
+            self.gpu = self.gpu[0]
 
     def system_info(self) -> dict:
         """return device informations like os, processer
@@ -36,20 +38,65 @@ class ProcessDetails:
             "total_cores": psutil.cpu_count(logical=True),
         }
 
-    def get_cpu_usage(self):
-        """get cpu usage"""
+    def get_size(self, size: int, suffix="B") -> str:
+        """
+        Scale bytes to its proper format- KB, MB, GB, TB and PB
+        Args:
+            size(int): size in bytes
+
+        Return:
+            str: return size of given bytes
+        """
+        factor = 1024
+        for unit in ["", "K", "M", "G", "T", "P"]:
+            if size < factor:
+                return f"{size:.2f}{unit}{suffix}"
+            size /= factor
+        return size
+
+    def get_cpu_usage(self) -> float:
+        """get cpu usage
+        Return:
+            float: return total cpu utilization
+        """
         cpu_utilization = self.process.cpu_percent()
         return cpu_utilization
 
-    def get_gpu_usage(self):
-        """return total gpu usage"""
-        return self.gpu.memoryFree
+    def get_gpu_usage(self) -> str:
+        """return total gpu usage
+        Return:
+            str: return total gpu usage in mb
+        """
+        if not isinstance(self.gpu, list):
+            return f"{self.gpu.memoryFree}MB"
+        return "0MB"
 
-    def get_memory_usage(self):
-        """get total memory usage"""
+    def get_memory_usage(self) -> str:
+        """get total memory usage
+        Return:
+            str: get total memory used
+        """
+        svmem = psutil.virtual_memory()
+        return self.get_size(svmem.used)
 
-    def get_gpu_temprature(self):
-        """return gpu temprature"""
+    def gpu_memory_free(self) -> str:
+        """get total memory free
+
+        Returns:
+            str: total memory in mb
+        """
+        if not isinstance(self.gpu, list):
+            return f"{self.gpu.memoryFree}MB"
+        return "0MB"
+
+    def get_gpu_temprature(self) -> str:
+        """return gpu temprature
+        Return:
+            str: temprature of gpu
+        """
+        if not isinstance(self.gpu, list):
+            return f"{self.gpu.temperature}*C"
+        return "0*C"
 
     def running_time(self, func) -> float:
         """running time decorator to caculate running time
